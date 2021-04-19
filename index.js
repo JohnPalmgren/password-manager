a2aa0aac40279117dcccd456cd2198c408639660
John <johnmpalmgren@gmail.com>
2021-04-19 06:38:20 +0100

    updeated index.js added media queries and readme

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-"use strict"
 const addbtn = document.getElementById("add");
 const backdrop = document.querySelector(".background");
 const passwordModal = document.querySelector(".pword-add");
-const passwordModalCancelBtn = document.querySelector(".pword-add button:last-of-type");
+const passwordModalCancelBtn = document.querySelector(
+  ".pword-add button:last-of-type"
+);
 const eye = document.querySelector(".fa-eye");
 const eyeSlash = document.querySelector(".fa-eye-slash");
 const passwordEl = document.getElementById("password");
@@ -10,76 +11,90 @@ const input = document.querySelectorAll(".pword-add input");
 const submit = document.querySelector(".pword-add button[type='submit");
 const generatorBtn = document.querySelector(".generator-btn");
 const generatorModel = document.getElementById("pword-generator");
-const generatorCancel = document.querySelector("#button-container button:last-of-type")
-const generatorConfirm = document.querySelector("#button-container button:first-of-type")
+const generatorCancel = document.querySelector(
+  "#button-container button:last-of-type"
+);
+const generatorConfirm = document.querySelector(
+  "#button-container button:first-of-type"
+);
 
-//dont need this
-const classIdTracker = [];
-
+/** Remove hidden property on password modal. */
 const addPasswordHandler = () => {
-    passwordModal.classList.remove("hidden");
-    backdrop.classList.remove("hidden");
+  passwordModal.classList.remove("hidden");
+  backdrop.classList.remove("hidden");
 };
 
+/**Add hidden property on password modal and reset defaults. */
 const removePasswordHandler = () => {
-    passwordModal.classList.add("hidden");
-    backdrop.classList.add("hidden");
-    input.forEach(field => field.value = "");
-    submit.disabled = true;
+  passwordModal.classList.add("hidden");
+  backdrop.classList.add("hidden");
+  input.forEach((field) => (field.value = ""));
+  submit.disabled = true;
 };
 
+/** Add hidden property to generator modal and reset defaults. */
 const removeGeneratorHandler = () => {
-    const form = generatorModel.querySelector("form");
-    form.reset();
-    generatorModel.classList.add("hidden");
+  const form = generatorModel.querySelector("form");
+  form.reset();
+  generatorModel.classList.add("hidden");
+  passwordModal.removeEventListener("click", removeGeneratorHandler);
 };
 
+/** Remove any displayed modals and hide backdrop. */
 const backdropHandler = () => {
-    removePasswordHandler();
-    removeGeneratorHandler();
-    backdrop.classList.add("hidden");
+  removePasswordHandler();
+  removeGeneratorHandler();
+  backdrop.classList.add("hidden");
 };
 
+/**
+ * Toggle the eye or eye-slash icon and toggle
+ * visibility of the password in password modal.
+ */
 const passwordVisibilityHandler = () => {
-    if (passwordEl.type === "password") {
-        eye.classList.add("hidden");
-        eyeSlash.classList.remove("hidden");
-        passwordEl.type="text";
-    } else {
-        eye.classList.remove("hidden");
-        eyeSlash.classList.add("hidden");
-        passwordEl.type="password";
-    };
+  if (passwordEl.type === "password") {
+    eye.classList.add("hidden");
+    eyeSlash.classList.remove("hidden");
+    passwordEl.type = "text";
+  } else {
+    eye.classList.remove("hidden");
+    eyeSlash.classList.add("hidden");
+    passwordEl.type = "password";
+  }
 };
 
+/** Enable add button in password modal when all fields have an input */
 const enableAddButtonHandler = () => {
-    let enableButton = true
-    input.forEach(field => {
-        if (!field.value) {
-            enableButton = false;
-        };
-    });
-    if (enableButton) {
-        submit.disabled = false;
-    };
+  let enableButton = true;
+  input.forEach((field) => {
+    if (!field.value) {
+      enableButton = false;
+    }
+  });
+  if (enableButton) {
+    submit.disabled = false;
+  }
 };
 
-// use index for class
-
+/**
+ * Add a row to the table using the input values from the password
+ * modal as cell values. Generate a random string as an id for the
+ * new elements. Add event listeners to newly generated buttons.
+ */
 const addTableRow = () => {
-    event.preventDefault();
-    const title = input[0].value;
-    const username = input[1].value;
-    const password = input[2].value;
-    const table = document.querySelector("table");
-    const randomString = "id" + Math.floor(Date.now()*Math.random()).toString()
-    const row = table.insertRow(1);
-    const cell1 = row.insertCell(0);
-    const cell2 = row.insertCell(1);
-    const cell3 = row.insertCell(2);
-    cell1.innerText = title;
-    cell2.innerText = username;
-    cell3.innerHTML = `
+  event.preventDefault();
+  const title = input[0].value;
+  const username = input[1].value;
+  const password = input[2].value;
+  const table = document.querySelector("table");
+  const randomString = "id" + Math.floor(Date.now() * Math.random()).toString();
+  const row = table.insertRow(1);
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  const cell3 = row.insertCell(2);
+  cell1.innerText = title;
+  cell2.innerText = username;
+  cell3.innerHTML = `
         <td>
             <div id="td-wrapper" class="${randomString}">
                 <span class="hidden text-password">${password}</span>
@@ -91,133 +106,170 @@ const addTableRow = () => {
             <button id="copy">Copy Password</button>
         </td>
     `;
-    const eye = document.querySelector(".table-eye")
-    const eyeSlash = document.querySelector(".table-slash")
-    const copyBtn = document.getElementById("copy")
-    eye.addEventListener("click", revealPasswordHandler.bind(self, randomString))
-    eyeSlash.addEventListener("click", revealPasswordHandler.bind(self, randomString))
-    copyBtn.addEventListener("click", copyPasswordHandler.bind(self, randomString))
-    classIdTracker.push(randomString);
-    removePasswordHandler();
+  const eye = document.querySelector(".table-eye");
+  const eyeSlash = document.querySelector(".table-slash");
+  const copyBtn = document.getElementById("copy");
+  eye.addEventListener("click", revealPasswordHandler.bind(self, randomString));
+  eyeSlash.addEventListener(
+    "click",
+    revealPasswordHandler.bind(self, randomString)
+  );
+  copyBtn.addEventListener(
+    "click",
+    copyPasswordHandler.bind(self, randomString)
+  );
+  removePasswordHandler();
 };
 
+/**
+ * Toggle password visibility of password in the table.
+ * @param {string} id class name for target element.
+ */
 const revealPasswordHandler = (id) => {
-    const tdWrapper = document.querySelector("."+id);
-    const eyeTable = tdWrapper.querySelector(".fa-eye")
-    const eyeTableSlash = tdWrapper.querySelector(".fa-eye-slash")
-    const revealed = tdWrapper.querySelector(".text-password")
-    const hiddenPwrd = tdWrapper.querySelector(".hidden-password")
-    if (revealed.classList.contains("hidden")) {
-        revealed.classList.remove("hidden");
-        hiddenPwrd.classList.add("hidden")
-        eyeTable.classList.add("hidden")
-        eyeTableSlash.classList.remove("hidden");
-    } else {
-        revealed.classList.add("hidden");
-        hiddenPwrd.classList.remove("hidden")
-        eyeTable.classList.remove("hidden")
-        eyeTableSlash.classList.add("hidden");
-    };
+  const tdWrapper = document.querySelector("." + id);
+  const eyeTable = tdWrapper.querySelector(".fa-eye");
+  const eyeTableSlash = tdWrapper.querySelector(".fa-eye-slash");
+  const revealed = tdWrapper.querySelector(".text-password");
+  const hiddenPwrd = tdWrapper.querySelector(".hidden-password");
+  if (revealed.classList.contains("hidden")) {
+    revealed.classList.remove("hidden");
+    hiddenPwrd.classList.add("hidden");
+    eyeTable.classList.add("hidden");
+    eyeTableSlash.classList.remove("hidden");
+  } else {
+    revealed.classList.add("hidden");
+    hiddenPwrd.classList.remove("hidden");
+    eyeTable.classList.remove("hidden");
+    eyeTableSlash.classList.add("hidden");
+  }
 };
 
+/**
+ * Copy password to clipboard.
+ * @param {string} id class name for target element.
+ */
 const copyPasswordHandler = (id) => {
-    const tdWrapper = document.querySelector("."+id);
-    const password = tdWrapper.querySelector(".password-input-box");
-    password.classList.remove("hidden");
-    password.select();
-    password.setSelectionRange(0, 99999);
-    document.execCommand("copy");
-    password.classList.add("hidden");
+  const tdWrapper = document.querySelector("." + id);
+  const password = tdWrapper.querySelector(".password-input-box");
+  password.classList.remove("hidden");
+  password.select();
+  password.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  password.classList.add("hidden");
 };
 
-const revealGenerator = () => {
-    generatorModel.classList.remove("hidden")
+/** Remove hidden property from generator modal */
+const revealGenerator = (event) => {
+  event.stopImmediatePropagation();
+  generatorModel.classList.remove("hidden");
+  passwordModal.addEventListener("click", removeGeneratorHandler);
 };
 
-// need to make sure 0 can come up and top number (may need +1 after list length)
-const randomItemFromArray = list => {
-    const index = Math.floor(Math.random() * list.length);
-    return list[index];
-}
+/**
+ * Returns randomly select an item from an array.
+ * @param {array} list
+ */
+const randomItemFromArray = (list) => {
+  const index = Math.floor(Math.random() * list.length);
+  return list[index];
+};
 
+/**
+ * Using characters and string length selected by the user generate a
+ * random string where each character has an equal chance of being selected.
+ * @returns {string} Randomly generated string.
+ */
 const generatorLogic = () => {
+  const uppercaseBox = document.querySelector(
+    "form input[type='checkbox']:first-of-type"
+  );
+  const lowercaseBox =
+    uppercaseBox.parentElement.nextElementSibling.lastElementChild
+      .previousElementSibling;
+  const numberBox =
+    lowercaseBox.parentElement.nextElementSibling.lastElementChild
+      .previousElementSibling;
+  const bracketBox =
+    numberBox.parentElement.nextElementSibling.lastElementChild
+      .previousElementSibling;
+  const specialBox =
+    bracketBox.parentElement.nextElementSibling.lastElementChild
+      .previousElementSibling;
+  const spaceBox =
+    specialBox.parentElement.nextElementSibling.lastElementChild
+      .previousElementSibling;
+  const length = document.getElementById("length").value;
 
-    const uppercaseBox = document.querySelector("form input[type='checkbox']:first-of-type")
-    const lowercaseBox = uppercaseBox.parentElement.nextElementSibling.lastChild.previousSibling;
-    const numberBox = lowercaseBox.parentElement.nextElementSibling.lastChild.previousSibling;
-    const bracketBox = numberBox.parentElement.nextElementSibling.lastChild.previousSibling;
-    const specialBox = bracketBox.parentElement.nextElementSibling.lastChild.previousSibling;
-    const spaceBox = specialBox.parentElement.nextElementSibling.lastChild.previousSibling;
-    const length = document.getElementById("length").value;
-    
-    const passwordList = [];
-    const selectedFields = {}
-      
-    const uppercase = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
-    const lowercase = Array.from("abcdefghijklmnopqrstuvwxyz");
-    const number = Array.from("0123456789");
-    const brackets = Array.from("<>[]{}()");
-    const special = Array.from("!\"#$%&'*+,.\\/:;=?@^`|~-_")
-    const space = [" "];
+  const passwordList = [];
+  const selectedFields = {};
 
-    let password = ""
-    let totalCharacters = 0;
+  const uppercase = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+  const lowercase = Array.from("abcdefghijklmnopqrstuvwxyz");
+  const number = Array.from("0123456789");
+  const brackets = Array.from("<>[]{}()");
+  const special = Array.from("!\"#$%&'*+,.\\/:;=?@^`|~-_");
+  const space = [" "];
 
+  let password = "";
+  let totalCharacters = 0;
 
-    // use switch case??
-    if (uppercaseBox.checked) {
-        totalCharacters += 26;
-        selectedFields.uppercase = [totalCharacters, uppercase];
-    } if (lowercaseBox.checked) {
-        totalCharacters += 26;
-        selectedFields.lowercase = [totalCharacters, lowercase];
-    } if (numberBox.checked) {
-        totalCharacters += 10;
-        selectedFields.number = [totalCharacters, number];
-    } if (bracketBox.checked) {
-        totalCharacters += 8;
-        selectedFields.brackets = [totalCharacters, brackets];
-    } if (specialBox.checked) {
-        totalCharacters += 24;
-        selectedFields.special = [totalCharacters, special];
-    } if (spaceBox.checked) {
-        totalCharacters += 1;
-        selectedFields.space = [totalCharacters, space];
-    };
+  if (uppercaseBox.checked) {
+    totalCharacters += 26;
+    selectedFields.uppercase = [totalCharacters, uppercase];
+  }
+  if (lowercaseBox.checked) {
+    totalCharacters += 26;
+    selectedFields.lowercase = [totalCharacters, lowercase];
+  }
+  if (numberBox.checked) {
+    totalCharacters += 10;
+    selectedFields.number = [totalCharacters, number];
+  }
+  if (bracketBox.checked) {
+    totalCharacters += 8;
+    selectedFields.brackets = [totalCharacters, brackets];
+  }
+  if (specialBox.checked) {
+    totalCharacters += 24;
+    selectedFields.special = [totalCharacters, special];
+  }
+  if (spaceBox.checked) {
+    totalCharacters += 1;
+    selectedFields.space = [totalCharacters, space];
+  }
 
-    for (let i = 0; i < length; i++) {
-        const selector = Math.floor(Math.random() * totalCharacters +1);
-        for (let field in selectedFields) {
-            if (selector <= selectedFields[field][0]) {
-                passwordList.push(randomItemFromArray(selectedFields[field][1]));
-                break;
-            };
-        };
-    };
-
-    for (const char of passwordList) {
-        password += char
+  for (let i = 0; i < length; i++) {
+    const selector = Math.floor(Math.random() * totalCharacters + 1);
+    for (const field in selectedFields) {
+      if (selector <= selectedFields[field][0]) {
+        passwordList.push(randomItemFromArray(selectedFields[field][1]));
+        break;
+      }
     }
-    return password;
+  }
+  for (const char of passwordList) {
+    password += char;
+  }
+  return password;
 };
 
+/** Add generated password to password field in password modal. */
 const generatePasswordHandler = () => {
-    const password = generatorLogic();
-    passwordEl.value = password;
-    enableAddButtonHandler();
-    removeGeneratorHandler();
-}
-
+  const password = generatorLogic();
+  passwordEl.value = password;
+  enableAddButtonHandler();
+  removeGeneratorHandler();
+};
 
 addbtn.addEventListener("click", addPasswordHandler);
 passwordModalCancelBtn.addEventListener("click", removePasswordHandler);
 backdrop.addEventListener("click", backdropHandler);
 eye.addEventListener("click", passwordVisibilityHandler);
 eyeSlash.addEventListener("click", passwordVisibilityHandler);
-input.forEach(field => {
-    field.addEventListener("keyup", enableAddButtonHandler);
+input.forEach((field) => {
+  field.addEventListener("keyup", enableAddButtonHandler);
 });
 submit.addEventListener("click", addTableRow);
-generatorBtn.addEventListener("click", revealGenerator);
 generatorCancel.addEventListener("click", removeGeneratorHandler);
 generatorConfirm.addEventListener("click", generatePasswordHandler);
+generatorBtn.addEventListener("click", revealGenerator);
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Password Manager (front end)
+
+## About
+This project provides the front end for a password manager. The user has the option to generate a random password from a selection of characters of their choosing. The passwords and user information is dynamically added to a table with JavaScript. There is also a build-in "copy password" function. 
+
+## Technologies used
+
+* HTML
+* CSS
+* JavaScript
